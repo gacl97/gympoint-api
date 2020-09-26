ec9207a62bafd718ae320c3da1146cff7b545810
Gustavo Augusto <gacl@ic.ufal.br>
2020-09-26 13:35:54 -0300

    Create middleware to AppError to use outside routes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,27 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(express.json());
+
+// Middleware to get errors outside routes;
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.status).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Servidor iniciado na porta 3333');
